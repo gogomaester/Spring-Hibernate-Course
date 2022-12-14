5b86754999050960acfcc2fec9520048708fb0b9
gogomaester <stolin.f@gmail.com>
2022-12-14 19:07:18 +0100

    Initial commit

---
+++ src/com/luv2code/springdemo/SoccerCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class SoccerCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "60 minutes Ronaldo style shooting practice.";
+	}	
+	
+
+}
