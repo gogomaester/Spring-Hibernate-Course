6cccce58522b30ff1a889c1bfadd852ed95e4092
gogomaester <stolin.f@gmail.com>
2022-12-14 21:06:24 +0100

    Constructor Injection finish

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -1,6 +1,17 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	// Empty constructor
+	public TrackCoach() {
+		
+	}
+	// Fortuneservice constructor
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -9,8 +20,7 @@ public class TrackCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Just do it: " + fortuneService.getFortune();
 	}
 
 }
